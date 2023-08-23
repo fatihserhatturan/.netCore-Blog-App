6ff5af6d151cf3398cd0e12f61637de9365a7f10
fatih serhat turan <95492309+fatihserhatturan@users.noreply.github.com>
2023-08-23 17:30:33 +0300

    Add files via upload

---
+++ EntityLayer/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+' denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+' bu öznitelik değerlerini değiştirin.
+
+' Bütünleştirilmiş kod özniteliklerinin değerlerini gözden geçirin
+
+<Assembly: AssemblyTitle("EntityLayer")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("EntityLayer")>
+<Assembly: AssemblyCopyright("Copyright ©  2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Bu proje COM'un kullanımına sunulursa aşağıdaki GUID, typelib kimliği içindir
+<Assembly: Guid("ad7f27af-947b-4946-a0df-ed7ee16af173")>
+
+' Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+'
+'      Ana Sürüm
+'      İkincil Sürüm 
+'      Yapı Numarası
+'      Düzeltme
+'
+' Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+' aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
